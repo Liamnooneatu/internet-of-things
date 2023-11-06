c946847855c54a09989584c6bf05704deb864d6c
Liam <G00420041@atu.ie>
2023-11-06 17:15:09 +0000

    first commit

---
+++ homepage.h
@@ -0,0 +1,7 @@
+// To create literal strings stored in flash memory enclose your HTML code between 
+// F(R"=====( HTML code here )=====");
+// If you have 1 reading then you probably have 2 literal strings
+// If you have 2 readings then you probably have 3 literal strings etc.
+
+String homePagePart1 = F(R"=====(CSS/HTML code here )=====");
+String homePagePart2 = F(R"=====(CSS/HTML code here)=====");
